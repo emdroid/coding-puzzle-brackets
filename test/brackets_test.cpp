f23d8e21997301923374ad9614e96f36c682e723
Emil Maskovsky <emil.maskovsky@gmail.com>
2014-10-29 16:52:32 +0100

    Add unit tests for various other components

--- test/brackets_test.cpp
+++ test/brackets_test.cpp
@@ -23,11 +23,176 @@
 #include "BracketPuzzle/StandardProcessor.hpp"
 #include "BracketPuzzle/MultithreadProcessor.hpp"
 #include "BracketPuzzle/StandardValidator.hpp"
+#include "BracketPuzzle/StreamInputReader.hpp"
+#include "BracketPuzzle/StreamResultWriter.hpp"
 #include "BracketPuzzle/VectorInputReader.hpp"
 #include "BracketPuzzle/VectorResultWriter.hpp"
 #include "BracketPuzzle/StandardResult.hpp"
 #include "BracketPuzzle/MultithreadResult.hpp"
 using namespace BracketPuzzle;
+
+
+BOOST_AUTO_TEST_SUITE(InputReader)
+
+
+BOOST_AUTO_TEST_CASE(StreamInputReaderTest)
+{
+    std::istringstream input("Line1\nLine2\n");
+    StreamInputReader reader(input);
+
+    std::string line;
+
+    // the first line
+    BOOST_CHECK(reader.readLine(line));
+    BOOST_CHECK_EQUAL(line, "Line1");
+    // the second line
+    BOOST_CHECK(reader.readLine(line));
+    BOOST_CHECK_EQUAL(line, "Line2");
+}
+
+
+BOOST_AUTO_TEST_CASE(VectorInputReaderTest)
+{
+    VectorInputReader reader;
+    reader.insert("Line1");
+    reader.insert("Line2");
+
+    std::string line;
+
+    // the first line
+    BOOST_CHECK(reader.readLine(line));
+    BOOST_CHECK_EQUAL(line, "Line1");
+    // the second line
+    BOOST_CHECK(reader.readLine(line));
+    BOOST_CHECK_EQUAL(line, "Line2");
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE(Result)
+
+
+BOOST_AUTO_TEST_CASE(StandardResultTest)
+{
+    StandardResult result1(true);
+    StandardResult result2(true);
+    StandardResult result3(false);
+
+    BOOST_CHECK(result1.isEqual(result2));
+    BOOST_CHECK(result2.isEqual(result1));
+    BOOST_CHECK(!result1.isEqual(result3));
+    BOOST_CHECK(!result3.isEqual(result2));
+    BOOST_CHECK(!result1.isLesserThan(result2));
+    BOOST_CHECK(!result2.isLesserThan(result1));
+    BOOST_CHECK(!result1.isLesserThan(result3));
+    BOOST_CHECK(result3.isLesserThan(result1));
+}
+
+
+BOOST_AUTO_TEST_CASE(MultithreadResultTest)
+{
+    MultithreadResult result1(0, true);
+    MultithreadResult result2(0, true);
+    MultithreadResult result3(1, false);
+
+    BOOST_CHECK(result1.isEqual(result2));
+    BOOST_CHECK(result2.isEqual(result1));
+    BOOST_CHECK(!result1.isEqual(result3));
+    BOOST_CHECK(!result3.isEqual(result2));
+    BOOST_CHECK(!result1.isLesserThan(result2));
+    BOOST_CHECK(!result2.isLesserThan(result1));
+    BOOST_CHECK(result1.isLesserThan(result3));
+    BOOST_CHECK(!result3.isLesserThan(result1));
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE(ResultWriter)
+
+
+BOOST_AUTO_TEST_CASE(StreamResultWriterTest)
+{
+    std::ostringstream out;
+    StreamResultWriter writer(out);
+
+    BOOST_CHECK(writer.writeResult(StandardResult(false)));
+    BOOST_CHECK_EQUAL(out.str(), "False\n");
+    BOOST_CHECK(writer.writeResult(StandardResult(true)));
+    BOOST_CHECK_EQUAL(out.str(), "False\nTrue\n");
+    BOOST_CHECK(writer.writeResult(MultithreadResult(5, true)));
+    BOOST_CHECK_EQUAL(out.str(), "False\nTrue\n5:True\n");
+}
+
+
+BOOST_AUTO_TEST_CASE(VectorResultWriterTest)
+{
+    VectorResultWriter writer;
+
+    BOOST_CHECK(writer.writeResult(StandardResult(true)));
+    BOOST_CHECK_EQUAL(writer.getData().size(), 1u);
+    BOOST_CHECK(writer.getData()[0]->isEqual(StandardResult(true)));
+
+    BOOST_CHECK(writer.writeResult(StandardResult(false)));
+    BOOST_CHECK_EQUAL(writer.getData().size(), 2u);
+    BOOST_CHECK(writer.getData()[0]->isEqual(StandardResult(true)));
+    BOOST_CHECK(writer.getData()[1]->isEqual(StandardResult(false)));
+
+    BOOST_CHECK(writer.writeResult(MultithreadResult(8, false)));
+    BOOST_CHECK_EQUAL(writer.getData().size(), 3u);
+    BOOST_CHECK(writer.getData()[0]->isEqual(StandardResult(true)));
+    BOOST_CHECK(writer.getData()[1]->isEqual(StandardResult(false)));
+    BOOST_CHECK(writer.getData()[2]->isEqual(MultithreadResult(8, false)));
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE(Validator)
+
+
+BOOST_AUTO_TEST_CASE(StandardValidatorTest)
+{
+    StandardValidator validator;
+
+    BOOST_CHECK(!validator.validate(""));
+    BOOST_CHECK(!validator.validate("a"));
+    BOOST_CHECK(!validator.validate(""));
+    BOOST_CHECK(!validator.validate("a"));
+    BOOST_CHECK(!validator.validate(" "));
+    BOOST_CHECK(!validator.validate("("));
+    BOOST_CHECK(!validator.validate(")"));
+    BOOST_CHECK(validator.validate("()"));
+    BOOST_CHECK(!validator.validate("(()"));
+    BOOST_CHECK(!validator.validate("()("));
+    BOOST_CHECK(!validator.validate("())"));
+    BOOST_CHECK(!validator.validate("( )"));
+    BOOST_CHECK(!validator.validate("()_"));
+    BOOST_CHECK(validator.validate("[]"));
+    BOOST_CHECK(validator.validate("{}"));
+    BOOST_CHECK(!validator.validate("({)}"));
+    BOOST_CHECK(validator.validate("({})"));
+    BOOST_CHECK(!validator.validate("([])"));
+    BOOST_CHECK(!validator.validate("(())"));
+    BOOST_CHECK(validator.validate("{[]}"));
+    BOOST_CHECK(!validator.validate("{()}"));
+    BOOST_CHECK(!validator.validate("{{}}"));
+    BOOST_CHECK(validator.validate("[()]"));
+    BOOST_CHECK(validator.validate("[{}]"));
+    BOOST_CHECK(validator.validate("[[]]"));
+    BOOST_CHECK(validator.validate("[[[]]]"));
+    BOOST_CHECK(!validator.validate("[([])]"));
+    BOOST_CHECK(validator.validate("[()()]"));
+    BOOST_CHECK(validator.validate("{[][()()]}"));
+    BOOST_CHECK(validator.validate("()()"));
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
 
 
 namespace
@@ -205,19 +370,22 @@ void testProcessing(
 
 BOOST_AUTO_TEST_SUITE(Processing)
 
-BOOST_AUTO_TEST_CASE(SingleThreadedProcessing)
+
+BOOST_AUTO_TEST_CASE(StandardProcessorTest)
 {
     testProcessing(
         StandardProcessor(),
         StandardResultValidator());
 }
 
-BOOST_AUTO_TEST_CASE(MultiThreadedProcessing)
+
+BOOST_AUTO_TEST_CASE(MultithreadProcessorTest)
 {
     testProcessing(
         MultithreadProcessor(),
         MultithreadResultValidator());
 }
+
 
 BOOST_AUTO_TEST_SUITE_END()
 
